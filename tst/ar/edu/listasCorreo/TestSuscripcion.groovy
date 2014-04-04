b9364413bac249083c2ac781e698cd9f15f66b96
Fernando Dodino <fernando.dodino@gmail.com>
2014-04-04 18:56:08 +0000

    Corregimos el método "tieneMalasPalabras" que no funcionaba (contains por un disjoint)

--- tst/ar/edu/listasCorreo/TestSuscripcion.groovy
+++ tst/ar/edu/listasCorreo/TestSuscripcion.groovy
@@ -4,6 +4,9 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
+import ar.edu.listaCorreo.Lista;
+import ar.edu.listaCorreo.Miembro;
+
 class TestSuscripcion {
 
 	Lista listaProfes
